8c6b69c41ed147731de16b701567ed5ffeca62b2
fliyyer <rahmatcube15@gmail.com>
2023-05-10 16:37:59 +0700

    rev4

--- js/custom-footer.js
+++ js/custom-footer.js
@@ -2,8 +2,14 @@
 class customFooter extends HTMLElement {
     connectedCallback() {
       this.innerHTML = `<footer>
+      <ul>
+        <li><a href="https://instagram.com/" target='_blank'><img src="../img/ig.png" alt="Tiktok"></a></li>
+        <li><a href="https://www.tiktok.com/" target='_blank'><img src="../img/tiktok.png" alt="Instagram"></a></li>
+      </ul>
       <p>Copyright © 2023 - ML-Strength</p>
-    </footer>`;
+    </footer>
+    
+    `;
     }
   }
   customElements.define('custom-footer', customFooter);
